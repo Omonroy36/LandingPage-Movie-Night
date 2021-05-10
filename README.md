e55d74ec01a308414abef87e00caa953dfba3c6b
Omonroy36 <monroyorlando.36@gmail.com>
2021-05-10 20:25:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,8 @@
+#  MOVIE NIGHT, Landing page in Javascript, HTML and CSS
+- Working hour: 2days
+- Dependency: Javascript, HTML and CSS without using frameworks or external libraries
+- 100% Responsive
+
+### Demo video
+
+[![Video Label](image.png)](https://youtu.be/ub1PQE9ANEo)
---
+++ public/js/index.js
@@ -0,0 +1,37 @@
+let button = document.querySelector("#fetch")
+button.addEventListener("click", () => {
+    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=8085c11d")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        showData(data.results)
+    })
+})
+
+const showData = (data) => {
+    let ul = document.querySelector("#lista")
+    for(let i =0 ; index < data.length; i++){
+    let li = document.createElement("li")
+    let text = document.createTextNode(data[i].name)
+    li.appendChile(text)
+    ul.appendChile(li)
+}
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+    }
+
+window.onscroll = () => {
+    var myNav = document.getElementById("myTopnav");
+    if (document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280) {
+    myNav.className += " navScroll";
+    } else {
+    myNav.className  = "topnav";
+    }
+};
